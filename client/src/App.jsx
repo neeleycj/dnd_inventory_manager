ae4a204c6a56a6ade1474977e0c6f241405e6446
Chandler <chandler.j.neeley@gmail.com>
2025-04-11 21:51:15 -0600

    Made a CharacterSheet component that will dynamically display props given

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import CharacterSheet from './CharacterSheet'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -21,6 +20,7 @@ function App() {
 
   return (
     <>
+<<<<<<< Updated upstream
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -41,7 +41,57 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+=======
+      
+>>>>>>> Stashed changes
       <button onClick={logout}>Logout</button>
+      <CharacterSheet 
+      character_name="Aragorn"
+      class="Fighter"
+      level="5"
+      race="human"
+      alignment="Nuetral Good"
+      ability_scores={{
+        str: 16,
+        dex: 14,
+        con: 15,
+        int: 10,
+        wis: 12,
+        cha: 8
+      }}
+      saving_throws={{
+        str: 5,
+        con: 4,
+        dex: 2,
+        wis: 1,
+        int: 0,
+        cha: -1
+      }}
+      combat_stats={{
+        armor_class: 17,
+        initiative: +2,
+        speed: "30 ft",
+        hit_points: 45,
+        hit_dice: "5d10"
+      }}
+      skills={{
+        athletics: "+5",
+        acrobatics: "+2",
+        stealth: "+2",
+        perception: "+3",
+        intimidation: "+1"
+      }}
+      equipment={{
+        weapons: ["Longsword", "Shortbow"],
+        armor: ["Chain Mail", "Shield"],
+        other: ["Explorer's Pack", "Healing Potion x2"]
+      }}
+      features_and_traits={{
+        features: ["Second Wind", "Action Surge"],
+        traits: ["Fighting Style: Defense", "Second Wind"],
+      }}
+      backstory= "Once a soldier in a forgotten war, now a wandering sword-for-hire with a strong sense of justice."
+      />
     </>
   )
 }
